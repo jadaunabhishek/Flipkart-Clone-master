6a37131704387b97963a07f146423831312f6c3f
Abhishek Jadaun <am3239@srmist.edu.in>
2024-01-13 15:16:43 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,36 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); 
+}
+
+let mslideIndex = 1;
+mshowSlides(mslideIndex);
+
+function plusSlides(n) {
+  mshowSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  mshowSlides(slideIndex = n);
+}
+
+function mshowSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
